1a1919b778946ab261636b48be923e71f62cf1cb
Robert Reed <robertreeddatasci@gmail.com>
2025-08-25 17:42:13 -0400

    Notes

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello World")
+from rapidfuzz import process, fuzz
+
+#Steps
+#1. Get players data from Sleeper API
+#2. Get ranks from FantasyPros CSV download
+#3. Add tags to rookies, lottery tickets or handcuffs. 
+#4. implement the simulator for draft day. Use rapidfuzz for easy player name entry (ADD FUNCTIONALITY TO REMOVE PLAYERS, OR ADD THEM BACK IF MISTAKE)
